cc9c59a0dd82244bd0a4fe9679822f97a5faf739
snoopy2199 <snoopy2199@gmail.com>
2016-11-13 13:48:18 +0800

    add github project link

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,10 +3,13 @@
         <div id="footer_comment" class="col-md-2 footer-link">
             <i class="fa fa-pencil-square-o" aria-hidden="true"></i> 評價
         </div>
-        <div id="footer_designer" class="col-md-4 col-md-offset-3 footer-word">
+        <div id="footer_designer" class="col-md-3 col-md-offset-3 footer-word">
             Designed by Tsai Shih-Yin
         </div>
-        <a id="footer_help" class="col-md-1 col-md-offset-1 footer-link" href="\help">常見問題</a>
+        <a class="col-md-1 col-md-offset-1 footer-link" href="https://github.com/snoopy2199/clear-note" target="_blank">
+            <img id="footer_github" src="{{ asset('img/GitHub-Mark-64px.png') }}">
+        </a>
+        <a id="footer_help" class="col-md-1 footer-link" href="\help">常見問題</a>
         <a id="footer_about" class="col-md-1 footer-link" href="\about">關於</a>
     </div>
 </div>
